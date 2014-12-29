8516dd88a9c6466a565d3ad91a0a3d8844f21634
yang-chao <y.yangchao@gmail.com>
2014-12-29 18:41:38 +0800

    add mainactivity2

---
+++ V2ex/app/src/main/java/com/price/v2ex/activity/MainActivity2.java
@@ -0,0 +1,83 @@
+package com.price.v2ex.activity;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.price.v2ex.R;
+import com.price.v2ex.adapter.FragmentStatePagerAdapter;
+import com.price.v2ex.fragment.HotFragment;
+
+public class MainActivity2 extends Activity implements HotFragment.OnFragmentInteractionListener {
+
+    private ViewPager mViewPager;
+    private FragmentStatePagerAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity2);
+
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        FragmentManager fm = getFragmentManager();
+        mAdapter = new FragmentStatePagerAdapter(fm) {
+            @Override
+            public Fragment getItem(int position) {
+                switch (position) {
+                    case 0:
+                        return HotFragment.newInstance("", "");
+                    default:
+                        return null;
+                }
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                switch (position) {
+                    case 0:
+                        return getString(R.string.main_tab_hot);
+                    default:
+                        return null;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return 1;
+            }
+        };
+        mViewPager.setAdapter(mAdapter);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main_activity2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(String id) {
+
+    }
+}
